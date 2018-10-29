74aaf59f8facee4c910f0b27fb35b531e30141a6
Philipp Trulson <philipp@trulson.de>
2018-10-29 21:14:04 +0100

    Add comment to exported function

--- vendors/lenovo/vendor.go
+++ vendors/lenovo/vendor.go
@@ -1,3 +1,17 @@
+// Copyright © 2018 Philipp Trulson <philipp@trulson.de>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package lenovo
 
 import (
@@ -8,11 +22,12 @@ import (
     "regexp"
     "strconv"
     "strings"
-    
+
     "github.com/buger/jsonparser"
     "github.com/pierrec/lz4"
 )
 
+// GetLatestBios polls the given website and returns version & link to file
 func GetLatestBios(website string) (string, string) {
     // Variable for saving the array index of BIOS download
     var arrayKey string
